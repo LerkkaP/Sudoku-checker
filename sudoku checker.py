a41c158b0774172d238d44b3bcbff176f7a550f7
Erik Peteri <peteri.erik@gmail.com>
2023-07-28 18:30:45 +0300

    code refactoring

--- sudoku checker.py
+++ sudoku checker.py
@@ -1,131 +1,43 @@
-class SudokuChecker():
+class SudokuChecker:
     def __init__(self, sudoku: list):
         self.sudoku = sudoku
 
-    def square_correct(self, sudoku: list):
-        list = []
-        column = 0
-        row = 0
-        border = column + 3
-        while True:
-            for i in range(0, 9):
-                start = sudoku[row][column]
-                if start > 0 and start in list:
-                    return False
-                list.append(start)
-                column += 1
-                if column == border:
-                    column = border - 3
-                    row += 1
-                if column == 2 and row == 2:
-                    if sudoku[2][2] in list and sudoku[2][2] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row = 0
-                    border = column + 3
-                    continue
-                if column == 5 and row == 2:
-                    if sudoku[2][5] in list and sudoku[2][5] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row = 0
-                    border = column + 3
-                    continue
-                if column == 2 and row == 5:
-                    if sudoku[5][2] in list and sudoku[5][2] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row -= 2
-                    border = column + 3
-                    continue
-                if column == 5 and row == 5:
-                    if sudoku[5][5] in list and sudoku[5][5] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row -= 2
-                    border = column + 3
-                    continue
-                if column == 8 and row == 5:
-                    if sudoku[5][8] in list and sudoku[5][8] > 0:
-                        return False
-                    list = []
-                    row += 1
-                    column = 0
-                    border = column + 3
-                    continue
-                if column == 2 and row == 8:
-                    if sudoku[8][2] in list and sudoku[8][2] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row -= 2
-                    border = column + 3
-                    continue
-                if column == 5 and row == 8:
-                    if sudoku[8][5] in list and sudoku[8][5] > 0:
-                        return False
-                    list = []
-                    column += 1
-                    row -= 2
-                    border = column + 3
-                    continue
-                if column == 8 and row == 2:
-                    if sudoku[2][8] in list and sudoku[2][8] > 0:
-                        return False
-                    list = []
-                    row += 1
-                    column = 0
-                    border = column + 3
-                    continue
-                if column == 8 and row == 8:
-                    if sudoku[8][8] in list and sudoku[8][8] > 0:
-                        return False
-                    return True
-
-    def column_correct(self, sudoku: list):
-        list = []
-        columm = 0
-        while columm < 9:
-            for row in sudoku:
-                if row[columm] > 0 and row[columm] in list:
-                    return False
-                list.append(row[columm])
-            if len(list) == 9:
-                columm += 1
-                list = []
-                continue
+    def square_correct(self) -> bool:
+        for row_offset in range(0, 9, 3):
+            for col_offset in range(0, 9, 3):
+                square_values = set()
+                for i in range(3):
+                    for j in range(3):
+                        value = self.sudoku[row_offset + i][col_offset + j]
+                        if value > 0 and value in square_values:
+                            return False
+                        square_values.add(value)
         return True
 
-    def row_correct(self, sudoku: list):
-        list = []
-        row = 0
-        while row < 9:
-            for luku in sudoku[row]:
-                if luku > 0 and luku in list:
+    def column_correct(self) -> bool:
+        for col in range(9):
+            column_values = set()
+            for row in range(9):
+                value = self.sudoku[row][col]
+                if value > 0 and value in column_values:
                     return False
-                list.append(luku)
-            if len(list) == 9:
-                row += 1
-                list = []
-                continue
+                column_values.add(value)
         return True
 
-    def check(self, sudoku: list):
-        if self.square_correct(sudoku) and self.column_correct(sudoku) and self.row_correct(sudoku):
-            return True
-        else:
-            return False
+    def row_correct(self) -> bool:
+        for row in self.sudoku:
+            row_values = set()
+            for value in row:
+                if value > 0 and value in row_values:
+                    return False
+                row_values.add(value)
+        return True
 
-    def solver(self, sudoku: list):
-        pass
-    
+    def check(self) -> bool:
+        return self.square_correct() and self.column_correct() and self.row_correct()
+
 if __name__ == "__main__":
-
-    sudoku = [
+    sudoku1 = [
         [2, 6, 7, 8, 3, 9, 5, 0, 4],
         [9, 0, 3, 5, 1, 0, 6, 0, 0],
         [0, 5, 1, 6, 0, 0, 8, 3, 9],
@@ -135,11 +47,11 @@ if __name__ == "__main__":
         [0, 0, 0, 4, 5, 7, 2, 6, 3],
         [3, 2, 0, 0, 8, 0, 0, 5, 7],
         [7, 4, 5, 0, 0, 3, 9, 0, 1]
-        ]
+    ]
 
-    print(SudokuChecker(sudoku).check(sudoku))
+    print(SudokuChecker(sudoku1).check()) 
 
-    sudoku = [
+    sudoku2 = [
         [9, 0, 0, 0, 8, 0, 3, 0, 0],
         [2, 0, 0, 2, 5, 0, 7, 0, 0],
         [0, 2, 0, 3, 0, 0, 0, 0, 4],
@@ -149,6 +61,6 @@ if __name__ == "__main__":
         [0, 0, 7, 8, 0, 3, 9, 0, 0],
         [0, 0, 1, 0, 0, 0, 0, 0, 3],
         [3, 0, 0, 0, 0, 0, 0, 0, 2]
-        ]
+    ]
 
-    print(SudokuChecker(sudoku).check(sudoku))
+    print(SudokuChecker(sudoku2).check())
